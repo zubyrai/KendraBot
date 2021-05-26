67ef693bfea95cf2566cdf4506f3e31da4490ed1
Zub7 <45486584+zubyrai@users.noreply.github.com>
2021-05-26 19:15:10 +0100

    Update chat.js

--- src/chat.js
+++ src/chat.js
@@ -8,11 +8,9 @@ componentDidMount(){
   
     (function(d, m){
         var kommunicateSettings = 
-            if(voiceInput == 'en-GB')
-            {"appId":"395d6c8e96ca1c3f069cb08e461fbd0a1","popupWidget":true,"automaticChatOpenOnNavigation":true, "voiceName":"Google UK English Male"}
-        else if (voiceInput == 'fr-FR')
-          {"appId":"395d6c8e96ca1c3f069cb08e461fbd0a1","popupWidget":true,"automaticChatOpenOnNavigation":true, "voiceName":"Google français"}
-                 
+          
+            {"appId":"395d6c8e96ca1c3f069cb08e461fbd0a1","popupWidget":true, "voiceInput": true,   "automaticChatOpenOnNavigation":true, "voiceName":"Google UK English Male", "Google français"};
+       
         var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
         s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
         var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
